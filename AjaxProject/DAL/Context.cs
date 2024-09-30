09cba733884eb687e92b89782b9541b18740f36f
CicekHuseyin <95969675+CicekHuseyin@users.noreply.github.com>
2024-09-30 16:56:40 +0300

    Customer tablosunda ki veriler üzerinden CRUD işlemleri yapıldı.

---
+++ AjaxProject/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProject.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-KR485FT\\SQLEXPRESS; initial catalog=AjaxDb; integrated security=true;");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
